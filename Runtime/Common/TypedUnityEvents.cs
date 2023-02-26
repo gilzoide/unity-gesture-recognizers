7a828f421b320edcd9344cd1f00b49d567325467
gilzoide <gilzoide@gmail.com>
2023-02-26 09:54:46 -0300

    Remove unused typed UnityEvent subclasses

--- Runtime/Common/TypedUnityEvents.cs
+++ Runtime/Common/TypedUnityEvents.cs
@@ -5,15 +5,6 @@ using UnityEngine.Events;
 
 namespace Gilzoide.GestureRecognizers.Common
 {
-    // primitives
-    [Serializable] public class UnityEventFloat : UnityEvent<float> {}
-    [Serializable] public class UnityEventVector2 : UnityEvent<Vector2> {}
-    
-    // enums
-    [Serializable] public class UnityEventSwipeDirection : UnityEvent<SwipeDirection> {}
-    [Serializable] public class UnityEventRectEdge : UnityEvent<RectEdge> {}
-    
-    // gesture structs
     [Serializable] public class UnityEventTapGesture : UnityEvent<TapGesture> {}
     [Serializable] public class UnityEventLongPressGesture : UnityEvent<LongPressGesture> {}
     [Serializable] public class UnityEventPanGesture : UnityEvent<PanGesture> {}
